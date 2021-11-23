b339b512345114b8161017d6f122d20be8a610cb
Diego Caponera <diego.caponera@gmail.com>
2021-11-23 12:39:32 +0100

    Display package information in homepage.

--- homepage/index.js
+++ homepage/index.js
@@ -1,10 +1,44 @@
 import path from "path";
 import chalk from "chalk";
 import { dirname } from "dirname-filename-esm";
+import { createRequire } from "module";
 import { readFile } from "fs/promises";
 import { load, render, log } from "../lib/index.js";
+import git from "git-rev-sync";
 
 const __dirname = dirname(import.meta);
+const require = createRequire(import.meta.url);
+
+const pkg = require(path.join("..", "package.json"));
+
+const config = {
+    title: "Goffre | Mini static site generator",
+    description:
+        "Goffre is a minimal static site generator available to the node.js ecosystem.",
+    nav: [
+        {
+            title: "Home",
+            slug: "index",
+            link: "/",
+        },
+        {
+            title: `Docs (v${pkg.version})`,
+            slug: "docs",
+            link: "/docs",
+        },
+        {
+            title: "See on Github",
+            link: "https://github.com/moonwave99/goffre",
+            external: true,
+        },
+    ],
+    domain: "https://moonwave99.github.io/goffre",
+    repo: "https://github.com/moonwave99/goffre",
+    author: {
+        name: "Diego Caponera",
+        link: "https://github.com/moonwave99",
+    },
+};
 
 const buildPath = path.join(__dirname, "dist");
 const dataPath = path.join(__dirname, "data");
@@ -13,7 +47,7 @@ const viewsPath = path.join(__dirname, "src", "views");
 (async () => {
     log(`Getting data from ${chalk.yellow(dataPath)}:`);
 
-    const { json, pages } = await load({ dataPath });
+    const { pages } = await load({ dataPath });
 
     try {
         const results = await render({
@@ -23,8 +57,8 @@ const viewsPath = path.join(__dirname, "src", "views");
             pages: [
                 ...pages,
                 {
-                    title: json.config.title,
-                    description: json.config.description,
+                    title: config.title,
+                    description: config.description,
                     slug: "index",
                     content: await readFile(
                         path.join("..", "README.md"),
@@ -33,8 +67,10 @@ const viewsPath = path.join(__dirname, "src", "views");
                 },
             ],
             locals: {
-                ...json,
-                domain: json.config.domain,
+                config,
+                pkg,
+                rev: git.short(".."),
+                domain: config.domain,
             },
         });
         log(`Generated ${chalk.yellow(results.length)} pages`);
