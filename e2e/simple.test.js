cf88a176130a2be46a3fb9e129ed80a6934b4af9
moonwave99 <diego.caponera@gmail.com>
2026-01-28 17:28:24 +0100

    Fix sitemap index URL

--- e2e/simple.test.js
+++ e2e/simple.test.js
@@ -9,255 +9,249 @@ const __dirname = dirname(import.meta);
 const buildPath = path.join(__dirname, "simple", "dist");
 const viewsPath = path.join(__dirname, "views");
 
-test.beforeEach(async () => await clean(buildPath));
-test.afterEach(async () => await clean(buildPath));
+test.beforeEach(() => clean(buildPath));
+test.afterEach(() => clean(buildPath));
 
 const pages = [
-    {
-        title: "Home",
-        slug: "index",
-        template: "page",
-        content: "This is a simple test",
-    },
-    {
-        title: "Blog",
-        slug: "blog",
-        template: "blog-index",
-    },
+  {
+    title: "Home",
+    slug: "index",
+    template: "page",
+    content: "This is a simple test",
+  },
+  {
+    title: "Blog",
+    slug: "blog",
+    template: "blog-index",
+  },
 ];
 
 const nav = [
-    {
-        title: "Home",
-        slug: "index",
-        link: "/",
-    },
-    {
-        title: "Blog",
-        slug: "/blog",
-    },
+  {
+    title: "Home",
+    slug: "index",
+    link: "/",
+  },
+  {
+    title: "Blog",
+    slug: "/blog",
+  },
 ];
 
 test.serial("e2e - simple", async (t) => {
-    const posts = generateItems(10);
+  const posts = generateItems(10);
 
-    await render({
-        buildPath,
-        viewsPath,
-        logLevel: "silent",
-        pages: [...pages, ...posts],
-        locals: {
-            posts,
-            config: { nav },
-        },
-    });
+  await render({
+    buildPath,
+    viewsPath,
+    logLevel: "silent",
+    pages: [...pages, ...posts],
+    locals: {
+      posts,
+      config: { nav },
+    },
+  });
 
-    const ss = new SuperStatic({ buildPath });
-    await ss.load();
+  const ss = new SuperStatic({ buildPath });
+  await ss.load();
 
-    // total page count
-    t.is(ss.pageCount(), posts.length + pages.length);
+  // total page count
+  t.is(ss.pageCount(), posts.length + pages.length);
 
-    // link generation
-    t.is(ss.getPage("index.html").$("nav .index").attr("href"), "/");
-    t.is(ss.getPage("index.html").$("nav .blog").attr("href"), "/blog");
+  // link generation
+  t.is(ss.getPage("index.html").$("nav .index").attr("href"), "/");
+  t.is(ss.getPage("index.html").$("nav .blog").attr("href"), "/blog");
 
-    // simple pages: existence, right nav class
-    pages.forEach((page) => {
-        const fileName = `${getSlug(page.slug, page)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
-    });
+  // simple pages: existence, right nav class
+  pages.forEach((page) => {
+    const fileName = `${getSlug(page.slug, page)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
+  });
 
-    // posts: existence, right title
-    posts.forEach((post) => {
-        const fileName = `${getSlug(post.slug, post)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("h1").html(), post.title);
-    });
+  // posts: existence, right title
+  posts.forEach((post) => {
+    const fileName = `${getSlug(post.slug, post)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("h1").html(), post.title);
+  });
 });
 
 test.serial("e2e - simple - uglyUrls", async (t) => {
-    const posts = generateItems(10);
+  const posts = generateItems(10);
 
-    await render({
-        buildPath,
-        viewsPath,
-        uglyUrls: true,
-        logLevel: "silent",
-        pages: [...pages, ...posts],
-        locals: {
-            posts,
-            config: { nav },
-        },
-    });
+  await render({
+    buildPath,
+    viewsPath,
+    uglyUrls: true,
+    logLevel: "silent",
+    pages: [...pages, ...posts],
+    locals: {
+      posts,
+      config: { nav },
+    },
+  });
 
-    const ss = new SuperStatic({ buildPath });
-    await ss.load();
+  const ss = new SuperStatic({ buildPath });
+  await ss.load();
 
-    // total page count
-    t.is(ss.pageCount(), posts.length + pages.length);
+  // total page count
+  t.is(ss.pageCount(), posts.length + pages.length);
 
-    // link generation
-    t.is(ss.getPage("index.html").$("nav .index").attr("href"), "/index.html");
-    t.is(ss.getPage("index.html").$("nav .blog").attr("href"), "/blog.html");
+  // link generation
+  t.is(ss.getPage("index.html").$("nav .index").attr("href"), "/index.html");
+  t.is(ss.getPage("index.html").$("nav .blog").attr("href"), "/blog.html");
 
-    // simple pages: existence, right nav class
-    pages.forEach((page) => {
-        const fileName = `${getSlug(page.slug, page)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
-    });
+  // simple pages: existence, right nav class
+  pages.forEach((page) => {
+    const fileName = `${getSlug(page.slug, page)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
+  });
 
-    // posts: existence, right title
-    posts.forEach((post) => {
-        const fileName = `${getSlug(post.slug, post)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("h1").html(), post.title);
-    });
+  // posts: existence, right title
+  posts.forEach((post) => {
+    const fileName = `${getSlug(post.slug, post)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("h1").html(), post.title);
+  });
 });
 
 test.serial("e2e - simple - prod", async (t) => {
-    const posts = generateItems(10);
-    const domain = "http://example.com";
+  const posts = generateItems(10);
+  const domain = "http://example.com";
 
-    await render({
-        buildPath,
-        viewsPath,
-        logLevel: "silent",
-        domain,
-        pages: [...pages, ...posts],
-        locals: {
-            posts,
-            config: { nav },
-        },
-        env: {
-            mode: "prod",
-        },
-    });
+  await render({
+    buildPath,
+    viewsPath,
+    logLevel: "silent",
+    domain,
+    pages: [...pages, ...posts],
+    locals: {
+      posts,
+      config: { nav },
+    },
+    env: {
+      mode: "prod",
+    },
+  });
 
-    const ss = new SuperStatic({ buildPath });
-    await ss.load();
+  const ss = new SuperStatic({ buildPath });
+  await ss.load();
 
-    // total page count
-    t.is(ss.pageCount(), posts.length + pages.length);
+  // total page count
+  t.is(ss.pageCount(), posts.length + pages.length);
 
-    // link generation
-    t.is(ss.getPage("index.html").$("nav .index").attr("href"), `${domain}/`);
-    t.is(
-        ss.getPage("index.html").$("nav .blog").attr("href"),
-        `${domain}/blog`
-    );
+  // link generation
+  t.is(ss.getPage("index.html").$("nav .index").attr("href"), `${domain}/`);
+  t.is(ss.getPage("index.html").$("nav .blog").attr("href"), `${domain}/blog`);
 
-    // simple pages: existence, right nav class
-    pages.forEach((page) => {
-        const fileName = `${getSlug(page.slug, page)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
-    });
+  // simple pages: existence, right nav class
+  pages.forEach((page) => {
+    const fileName = `${getSlug(page.slug, page)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
+  });
 
-    // posts: existence, right title
-    posts.forEach((post) => {
-        const fileName = `${getSlug(post.slug, post)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("h1").html(), post.title);
-    });
+  // posts: existence, right title
+  posts.forEach((post) => {
+    const fileName = `${getSlug(post.slug, post)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("h1").html(), post.title);
+  });
 });
 
 test.serial("e2e - simple - prod - uglyurls", async (t) => {
-    const posts = generateItems(10);
-    const domain = "http://example.com";
+  const posts = generateItems(10);
+  const domain = "http://example.com";
 
-    await render({
-        buildPath,
-        viewsPath,
-        uglyUrls: true,
-        logLevel: "silent",
-        domain,
-        pages: [...pages, ...posts],
-        locals: {
-            posts,
-            config: { nav },
-        },
-        env: {
-            mode: "prod",
-        },
-    });
+  await render({
+    buildPath,
+    viewsPath,
+    uglyUrls: true,
+    logLevel: "silent",
+    domain,
+    pages: [...pages, ...posts],
+    locals: {
+      posts,
+      config: { nav },
+    },
+    env: {
+      mode: "prod",
+    },
+  });
 
-    const ss = new SuperStatic({ buildPath });
-    await ss.load();
+  const ss = new SuperStatic({ buildPath });
+  await ss.load();
 
-    // total page count
-    t.is(ss.pageCount(), posts.length + pages.length);
+  // total page count
+  t.is(ss.pageCount(), posts.length + pages.length);
 
-    // link generation
-    t.is(
-        ss.getPage("index.html").$("nav .index").attr("href"),
-        `${domain}/index.html`
-    );
-    t.is(
-        ss.getPage("index.html").$("nav .blog").attr("href"),
-        `${domain}/blog.html`
-    );
+  // link generation
+  t.is(
+    ss.getPage("index.html").$("nav .index").attr("href"),
+    `${domain}/index.html`,
+  );
+  t.is(
+    ss.getPage("index.html").$("nav .blog").attr("href"),
+    `${domain}/blog.html`,
+  );
 
-    // simple pages: existence, right nav class
-    pages.forEach((page) => {
-        const fileName = `${getSlug(page.slug, page)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
-    });
+  // simple pages: existence, right nav class
+  pages.forEach((page) => {
+    const fileName = `${getSlug(page.slug, page)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
+  });
 
-    // posts: existence, right title
-    posts.forEach((post) => {
-        const fileName = `${getSlug(post.slug, post)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("h1").html(), post.title);
-    });
+  // posts: existence, right title
+  posts.forEach((post) => {
+    const fileName = `${getSlug(post.slug, post)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("h1").html(), post.title);
+  });
 });
 
 test.serial("e2e - simple - prod in subfolder", async (t) => {
-    const posts = generateItems(10);
-    const domain = "http://example.com/subfolder";
+  const posts = generateItems(10);
+  const domain = "http://example.com/subfolder";
 
-    await render({
-        buildPath,
-        viewsPath,
-        logLevel: "silent",
-        domain,
-        pages: [...pages, ...posts],
-        locals: {
-            posts,
-            config: { nav },
-        },
-        env: {
-            mode: "prod",
-        },
-    });
+  await render({
+    buildPath,
+    viewsPath,
+    logLevel: "silent",
+    domain,
+    pages: [...pages, ...posts],
+    locals: {
+      posts,
+      config: { nav },
+    },
+    env: {
+      mode: "prod",
+    },
+  });
 
-    const ss = new SuperStatic({ buildPath });
-    await ss.load();
+  const ss = new SuperStatic({ buildPath });
+  await ss.load();
 
-    // total page count
-    t.is(ss.pageCount(), posts.length + pages.length);
+  // total page count
+  t.is(ss.pageCount(), posts.length + pages.length);
 
-    // link generation
-    t.is(ss.getPage("index.html").$("nav .index").attr("href"), `${domain}/`);
-    t.is(
-        ss.getPage("index.html").$("nav .blog").attr("href"),
-        `${domain}/blog`
-    );
+  // link generation
+  t.is(ss.getPage("index.html").$("nav .index").attr("href"), `${domain}/`);
+  t.is(ss.getPage("index.html").$("nav .blog").attr("href"), `${domain}/blog`);
 
-    // simple pages: existence, right nav class
-    pages.forEach((page) => {
-        const fileName = `${getSlug(page.slug, page)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
-    });
+  // simple pages: existence, right nav class
+  pages.forEach((page) => {
+    const fileName = `${getSlug(page.slug, page)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("nav .current").html(), page.title);
+  });
 
-    // posts: existence, right title
-    posts.forEach((post) => {
-        const fileName = `${getSlug(post.slug, post)}.html`;
-        t.true(ss.hasPage(fileName));
-        t.is(ss.getPage(fileName).$("h1").html(), post.title);
-    });
+  // posts: existence, right title
+  posts.forEach((post) => {
+    const fileName = `${getSlug(post.slug, post)}.html`;
+    t.true(ss.hasPage(fileName));
+    t.is(ss.getPage(fileName).$("h1").html(), post.title);
+  });
 });
