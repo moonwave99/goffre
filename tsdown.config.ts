a8b09318ff4aff9074c17f28ec87f4a48911756b
moonwave99 <diego.caponera@gmail.com>
2026-02-22 09:42:54 +0100

    Fix build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "tsdown";
 export default defineConfig([
   {
     entry: ["lib/index.ts"],
+    inlineOnly: false,
   },
   {
     entry: ["lib/generator.ts"],
+    inlineOnly: false,
   },
 ]);
